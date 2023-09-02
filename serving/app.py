ccbb2bb18abacac0003f09208d7c89c27e248e39
nico-usf <nthiebaut@usfca.edu>
2023-09-02 01:40:48 -0700

    add model to app

--- serving/app.py
+++ serving/app.py
@@ -1,154 +1,80 @@
 from functools import partial
 from io import BytesIO
-from typing import TYPE_CHECKING
 
+import torch
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
 from loguru import logger
 from PIL import Image
+from pydantic import BaseModel
+from torch.nn.functional import softmax
 from transformers import ViTImageProcessor
 
-if TYPE_CHECKING:
-    from torch import tensor
+
+class ClassPredictions(BaseModel):
+    predictions: dict[str, float]
 
 
-labels = [
-    "apple_pie",
-    "baby_back_ribs",
-    "baklava",
-    "beef_carpaccio",
-    "beef_tartare",
-    "beet_salad",
-    "beignets",
-    "bibimbap",
-    "bread_pudding",
-    "breakfast_burrito",
-    "bruschetta",
-    "caesar_salad",
-    "cannoli",
-    "caprese_salad",
-    "carrot_cake",
-    "ceviche",
-    "cheese_plate",
-    "cheesecake",
-    "chicken_curry",
-    "chicken_quesadilla",
-    "chicken_wings",
-    "chocolate_cake",
-    "chocolate_mousse",
-    "churros",
-    "clam_chowder",
-    "club_sandwich",
-    "crab_cakes",
-    "creme_brulee",
-    "croque_madame",
-    "cup_cakes",
-    "deviled_eggs",
-    "donuts",
-    "dumplings",
-    "edamame",
-    "eggs_benedict",
-    "escargots",
-    "falafel",
-    "filet_mignon",
-    "fish_and_chips",
-    "foie_gras",
-    "french_fries",
-    "french_onion_soup",
-    "french_toast",
-    "fried_calamari",
-    "fried_rice",
-    "frozen_yogurt",
-    "garlic_bread",
-    "gnocchi",
-    "greek_salad",
-    "grilled_cheese_sandwich",
-    "grilled_salmon",
-    "guacamole",
-    "gyoza",
-    "hamburger",
-    "hot_and_sour_soup",
-    "hot_dog",
-    "huevos_rancheros",
-    "hummus",
-    "ice_cream",
-    "lasagna",
-    "lobster_bisque",
-    "lobster_roll_sandwich",
-    "macaroni_and_cheese",
-    "macarons",
-    "miso_soup",
-    "mussels",
-    "nachos",
-    "omelette",
-    "onion_rings",
-    "oysters",
-    "pad_thai",
-    "paella",
-    "pancakes",
-    "panna_cotta",
-    "peking_duck",
-    "pho",
-    "pizza",
-    "pork_chop",
-    "poutine",
-    "prime_rib",
-    "pulled_pork_sandwich",
-    "ramen",
-    "ravioli",
-    "red_velvet_cake",
-    "risotto",
-    "samosa",
-    "sashimi",
-    "scallops",
-    "seaweed_salad",
-    "shrimp_and_grits",
-    "spaghetti_bolognese",
-    "spaghetti_carbonara",
-    "spring_rolls",
-    "steak",
-    "strawberry_shortcake",
-    "sushi",
-    "tacos",
-    "takoyaki",
-    "tiramisu",
-    "tuna_tartare",
-    "waffles",
-]
+app = FastAPI()
+
 
 model_name_or_path = "google/vit-base-patch16-224-in21k"
 feature_extractor = ViTImageProcessor.from_pretrained(model_name_or_path)
 preprocessor = partial(feature_extractor, return_tensors="pt")
 
-#####
-# Exercise: Write the model loading code.
-#####
 
-app = FastAPI()
-
-
-def preprocess_image(image: Image.Image) -> tensor:
+def preprocess_image(image: Image.Image) -> torch.tensor:
     return preprocessor([image])["pixel_values"]
 
 
-def read_imagefile(file: File) -> Image.Image:
-    image = Image.open(BytesIO(file))
-    return image
+def read_imagefile(file: bytes) -> Image.Image:
+    return Image.open(BytesIO(file))
+
+
+MODEL_PATH = "/Users/nthiebaut/Downloads/model.ckpt"
+
+
+def load_model(model_path: str = MODEL_PATH) -> torch.nn.Module:
+    checkpoint = torch.load(model_path, map_location=torch.device("cpu"))
+    model = checkpoint["hyper_parameters"]["model"]
+    labels = checkpoint["hyper_parameters"]["label_names"]
+    model.eval()  # To set up inference (disable dropout, layernorm, etc.)
+    return model, labels
+
+
+model, labels = load_model()
+
+
+def predict(x: torch.tensor, labels: list = labels) -> dict:
+    logits = model(x).logits
+    probas = softmax(logits, dim=1)
+
+    values, indices = torch.topk(probas[0], 5)
+    return_dict = {labels[int(i)]: float(v) for i, v in zip(indices, values)}
+    return return_dict
 
 
 @app.get("/")
-def healthcheck() -> dict:
+def get_root() -> dict:
     logger.info("Received request on the root endpoint")
     return {"status": "ok"}
 
 
-@app.post("/predict")
-async def predict_api(file: UploadFile = File(...)):
-    #####
-    # Exercise: Write the prediction endpoint, following the signature defined above.
-    #####
-    pass
+@app.post("/predict", response_model=ClassPredictions)
+async def predict_api(file: UploadFile = File(...)) -> ClassPredictions:
+    logger.info(f"Predict endpoint received image {file.filename}")
+
+    file_extension = file.filename.split(".")[-1]
+    valid_extensions = ("jpg", "jpeg", "png")
+    if file_extension not in valid_extensions:
+        raise TypeError(
+            f"File extension for {file.filename} should be one of {valid_extensions}"
+        )
+
+    image = read_imagefile(await file.read())
+    x = preprocess_image(image)
+    return ClassPredictions(predictions=predict(x))
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
