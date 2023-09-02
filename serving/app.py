edf24197ae8a8a9dff87dc47d39a25a24e83f460
nico-usf <nthiebaut@usfca.edu>
2023-09-02 00:19:30 -0700

    rename apps

--- serving/app.py
+++ serving/app.py
@@ -1,34 +1,153 @@
-from typing import Optional
+from functools import partial
+from io import BytesIO
+from typing import TYPE_CHECKING
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from loguru import logger
-from pydantic import BaseModel, Field
+from PIL import Image
+from transformers import ViTImageProcessor
+
+if TYPE_CHECKING:
+    from torch import tensor
+
+
+labels = [
+    "apple_pie",
+    "baby_back_ribs",
+    "baklava",
+    "beef_carpaccio",
+    "beef_tartare",
+    "beet_salad",
+    "beignets",
+    "bibimbap",
+    "bread_pudding",
+    "breakfast_burrito",
+    "bruschetta",
+    "caesar_salad",
+    "cannoli",
+    "caprese_salad",
+    "carrot_cake",
+    "ceviche",
+    "cheese_plate",
+    "cheesecake",
+    "chicken_curry",
+    "chicken_quesadilla",
+    "chicken_wings",
+    "chocolate_cake",
+    "chocolate_mousse",
+    "churros",
+    "clam_chowder",
+    "club_sandwich",
+    "crab_cakes",
+    "creme_brulee",
+    "croque_madame",
+    "cup_cakes",
+    "deviled_eggs",
+    "donuts",
+    "dumplings",
+    "edamame",
+    "eggs_benedict",
+    "escargots",
+    "falafel",
+    "filet_mignon",
+    "fish_and_chips",
+    "foie_gras",
+    "french_fries",
+    "french_onion_soup",
+    "french_toast",
+    "fried_calamari",
+    "fried_rice",
+    "frozen_yogurt",
+    "garlic_bread",
+    "gnocchi",
+    "greek_salad",
+    "grilled_cheese_sandwich",
+    "grilled_salmon",
+    "guacamole",
+    "gyoza",
+    "hamburger",
+    "hot_and_sour_soup",
+    "hot_dog",
+    "huevos_rancheros",
+    "hummus",
+    "ice_cream",
+    "lasagna",
+    "lobster_bisque",
+    "lobster_roll_sandwich",
+    "macaroni_and_cheese",
+    "macarons",
+    "miso_soup",
+    "mussels",
+    "nachos",
+    "omelette",
+    "onion_rings",
+    "oysters",
+    "pad_thai",
+    "paella",
+    "pancakes",
+    "panna_cotta",
+    "peking_duck",
+    "pho",
+    "pizza",
+    "pork_chop",
+    "poutine",
+    "prime_rib",
+    "pulled_pork_sandwich",
+    "ramen",
+    "ravioli",
+    "red_velvet_cake",
+    "risotto",
+    "samosa",
+    "sashimi",
+    "scallops",
+    "seaweed_salad",
+    "shrimp_and_grits",
+    "spaghetti_bolognese",
+    "spaghetti_carbonara",
+    "spring_rolls",
+    "steak",
+    "strawberry_shortcake",
+    "sushi",
+    "tacos",
+    "takoyaki",
+    "tiramisu",
+    "tuna_tartare",
+    "waffles",
+]
+
+model_name_or_path = "google/vit-base-patch16-224-in21k"
+feature_extractor = ViTImageProcessor.from_pretrained(model_name_or_path)
+preprocessor = partial(feature_extractor, return_tensors="pt")
+
+#####
+# Exercise: Write the model loading code.
+#####
 
 app = FastAPI()
 
 
-class TypedRequest(BaseModel):
-    strings: list[str] = Field(..., example=["Start", "Next"])
+def preprocess_image(image: Image.Image) -> tensor:
+    return preprocessor([image])["pixel_values"]
 
 
-class TypedResponse(BaseModel):
-    data: list[str]
-    opt: Optional[dict[str, float]] = None
+def read_imagefile(file: File) -> Image.Image:
+    image = Image.open(BytesIO(file))
+    return image
 
 
 @app.get("/")
-def read_root() -> dict:
+def healthcheck() -> dict:
     logger.info("Received request on the root endpoint")
     return {"status": "ok"}
 
 
-@app.post("/predict", response_model=TypedResponse)
-async def predict(request: TypedRequest) -> TypedResponse:
-    logger.info(f"predict endpoint received request: {request}")
-    request_strings = request.strings
-    response = {"data": [f"{s}_response" for s in request_strings]}
-    return TypedResponse(**response)
+@app.post("/predict")
+async def predict_api(file: UploadFile = File(...)):
+    #####
+    # Exercise: Write the prediction endpoint, following the signature defined above.
+    #####
+    pass
 
 
 if __name__ == "__main__":
